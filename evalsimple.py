0a7431228ad5262e2233be7587a5e4ef15309062
unislave <rickstratton@gmail.com>
2020-01-22 20:46:44 -0500

    lets try this again

--- evalsimple.py
+++ evalsimple.py
@@ -1,10 +1,12 @@
 # simple version of the fitness function, for testing other functions
+# input is a 'solution', represented as a 1xn array
+# the function currently adds the values together and returns the sum
 
 import numpy
 
 def fitness(solution):
     """This function evaluates the fitness of a candidate solution"""
-    fitvalue = -1
+    fitvalue = 0
     for i in range(len(solution)):
         fitvalue += solution[i]
     return fitvalue
--- survivor_selection.py
+++ survivor_selection.py
@@ -1,1 +1,16 @@
-""" Survivor Selection to be written by Rick """
+def cull(numretirees, fitnesses):
+
+    retireelist=[0]*numretirees
+    for i in range(numretirees):
+        min = 99999
+        for j,value in enumerate(fitnesses):
+            if value<min:
+                min = value
+                index = j
+        retireelist[i]=index
+        fitnesses[index]=99999
+    #        print(fitnesses)
+
+    return retireelist
+
+# print(cull([13, 20, 14, 11, 21],1))
