83bf2841a1588cdf1bb531a3cd390841c1192c20
Brooke <brooke.clouston@gmail.com>
2020-01-29 20:38:18 -0500

    adding new implmentation of data structure, rewriting initalization function

---
+++ constraints.py
@@ -0,0 +1,11 @@
+"""
+This script contains constraint information to be used by the genetic algorithm 
+"""
+
+numgenmax = 5 # maximum number of generations (avoid infinite loop)
+#profs_num = 3 # number of profs 
+#courses_num = 10 # genes / number of courses
+#sections = ["Monday 9am", "Monday 11am", "Monday 1pm", "Monday 3pm"] # chromosomes / number of time sections
+pop_size = 10 # population size
+parents = pop_size // 5 # 20% of the population each generation becomes parents
+retirees = pop_size // 5 # 20% of the population each generation 'retires' and leaves the population
--- evaluation.py
+++ evaluation.py
@@ -12,7 +12,8 @@ Calculates a basic fitness value for a candidate solution based on 100 - number 
 :param time_table:         Existing time table (not currently used, but could be interesting to play with)
 :returns:                  Integer representing computed fitness
 """
-def basic_fitness(candidate_solution, time_table):
-    duplicates = set([x for x in candidate_solution if candidate_solution.count(x) > 1])
-    return 100 - len(duplicates)
+def basic_fitness(candidate_solution):
+    return 100
+    #duplicates = set([x for x in candidate_solution if candidate_solution.count(x) > 1])
+    #return 100 - len(duplicates)
 
