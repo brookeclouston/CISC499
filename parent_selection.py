408af93e985e008a94d0c731ac1e6076e414085e
Brooke <brooke.clouston@gmail.com>
2020-02-09 19:12:23 -0500

    adding random parent selection function

--- parent_selection.py
+++ parent_selection.py
@@ -13,8 +13,10 @@ then calls sub-function. Returns a list of index values
 """
 def select_parents(num_parents, fitnesses):
     parent_list = [0] * num_parents
+    if constraints.parent_selection == "random":
+        return select_parents_random(num_parents, fitnesses)
     for parent in range(num_parents):
-        if constraints.parent_selection == 'tournament':
+        if constraints.parent_selection == "tournament":
             parent_list[parent] = select_parents_tournament(constraints.tournament_y, fitnesses)
     return parent_list
 
@@ -28,7 +30,7 @@ randomly chosen parents, and the index of the parent with the max fitness is ret
 """
 def select_parents_tournament(y_parents, fitnesses):
     parent_sublist = [0] * y_parents
-    index = []
+    index = 0
     for i in range(y_parents):
         parent_sublist[i] = randint(0,len(fitnesses))
     max = 0
@@ -38,5 +40,22 @@ def select_parents_tournament(y_parents, fitnesses):
             index = j
     return index
 
+
+"""
+Function: select_parents_random
+Selects parents randomly based off of index positions. Does not allow for duplicate parent values.
+:param num_parents: Number of parents to randomly choose
+:param fitnesses:   List of fitnesses (used to calculate possible range of solutions)
+:returns:           List of indexes of randomly selected parents
+"""
+def select_parents_random(num_parents, fitnesses):
+    parents = []
+    for i in range(num_parents):
+        rand_parent = randint(0, len(fitnesses))
+        if rand_parent not in parents:
+            parents.append(rand_parent)
+            break 
+    return parents
+    
 
 #TODO: Brooke implement different versions of parent selection -> random, weighted sum (roulette)
